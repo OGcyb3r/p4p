33d2a3c522e37c709bb19afc647b428e1c80f8d9
OGcyb3r <48766198+OGcyb3r@users.noreply.github.com>
2020-05-21 20:42:01 +0300

    Update string generator.php

--- string generator.php
+++ string generator.php
@@ -1,15 +1,17 @@
 <?php
-function api($ss){
-          $l=$ss;
-          $api="";
-          $r= "qwertyuioplkjhgfdsazxcvbnm1234567890";
-            srand((double)microtime()*1000000);
-            for($i=0; $i<$l; $i++) {
-              $api.= $r[rand()%strlen($r)];
-            }
-            return $api;
-      }
-echo api($_GET['cmd']);  
-#localhost/string_generator.php?cmd=3 = pJe 
+function api($need){
+  $api="";
+  $r1= "qwertyuioplkjhgfdsazxcvbnm";
+  $r= "qwertyuioplkjhgfdsazxcvbnm1234567890._-";
+  srand((double)microtime()*1000000);
+  for($i=0; $i<$need; $i++) {
+            #add more as u wish
+    $api= $r1[rand()%strlen($r1)].$r[rand()%strlen($r)].$r[rand()%strlen($r)];
+    echo "$api<br>";
+  }
+}
+api($_GET['cmd'])
+
+#localhost/string_generator.php?cmd=100 = pJe
 
 ?>
